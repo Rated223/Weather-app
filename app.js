87554ccb6944ade610f2cf84e85e2a730802eb00
Aldo Cedillo <mr_jenkinshlcn@hotmail.com>
2019-03-29 11:07:08 -0600

    parasing the data in the request

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/9a092f4f99fcbfa3dc1bf1c24feb3eb6/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/9a092f4f99fcbfa3dc1bf1c24feb3eb6/37.8267,-122.4233?units=si';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.dir(data.currently, {depth: null});
+request({ url: url, json: true }, (error, response) => {
+  //console.log(response.body.currently);
+  console.log(`${response.body.daily.data[0]. summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain`);
 });
