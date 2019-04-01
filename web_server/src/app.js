e0d4eef38392a8e23da2d1bc02f7579caa4f2cce
Aldo Cedillo <mr_jenkinshlcn@hotmail.com>
2019-04-01 16:29:40 -0600

    add express and hbs for dynamic templates

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,24 +1,42 @@
+const path = require('path');
 const express = require('express');
 
+console.log(__dirname);
+console.log(path.join(__dirname, '../public'));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath)); // this routes gonna have priority
 
 app.get('', (req, res) => {
-  res.send('Hello express');
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page');
-})
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Andrew Mead'
+  });
+});
 
 app.get('/about', (req, res) => {
-  res.send('about page');
-})
+  res.render('about', {
+    title: 'About',
+    name: 'Andrew Mead'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    helpText: 'This is a help text gueguegue',
+    name: 'Andrew Mead'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send('weather page');
 })
 
-app.listen(3005, () => {
-  console.log('server running in port 3005');
+app.listen(3006, () => {
+  console.log('server running in port 3006');
 })
 
