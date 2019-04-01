69f8087234980128592085b958222e1a9e81d43a
Aldo Cedillo <mr_jenkinshlcn@hotmail.com>
2019-04-01 17:04:18 -0600

    add partials templates to reuse html code

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirectoryPath)); // this routes gonna have priority
 
 app.get('', (req, res) => {
