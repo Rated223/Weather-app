c1f20c705fefdda78acb4069c075fe0668c97309
Aldo Cedillo <mr_jenkinshlcn@hotmail.com>
2019-04-02 17:10:06 -0600

    add information display in the browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const message1 = document.querySelector('#error');
+const message2 = document.querySelector('#weather');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
+  message1.textContent = 'Loading...';  
+        message2.textContent = ""; 
   fetch(`http://localhost:3006/weather?address=${location}`).then( (response) => {
     response.json().then( (data) => {
       if (data.error) {
-        return console.log(data.error);  
+        message1.textContent = data.error;  
+        message2.textContent = ""; 
+      } else { 
+        message1.textContent = data.location; 
+        message2.textContent = data.forecast; 
       }
-      console.log(data.location);
-      console.log(data.forecast);
     });
   });
 } );
