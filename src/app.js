9ea1fd5112bd2e20d42dd594355e91fe7a6f68d7
Aldo Cedillo <mr_jenkinshlcn@hotmail.com>
2019-04-03 11:32:46 -0600

    Setup app for deploy in Heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
 
 const app = express();
+
+const port = process.env.PORT || 3006;
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
@@ -90,7 +93,7 @@ app.get('*', (req, res) => {
   })
 });
 
-app.listen(3006, () => {
-  console.log('server running in port 3006');
+app.listen(port, () => {
+  console.log(`server running in port ${port}`);
 })
 
